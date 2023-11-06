e798dab3c9e859b0d3d3e6bf410b1845329e1dac
kotysky <kotysky@DESKTOP-IK71H8Q>
2023-11-06 09:37:11 +0000

    h1

--- contracts/RamdomIpfsNft.sol
+++ contracts/RamdomIpfsNft.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract RamdomIpfsNft is VRFConsumerBaseV2 {
+contract RamdomIpfsNft() is VRFConsumerBaseV2 {
     // user has to pay to mint an NFT
 
     function requestNft() public {}
